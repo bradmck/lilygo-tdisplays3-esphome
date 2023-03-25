20bd76778caca71eaf096ebe5244a76acab7cbc0
bradmck <bradmck@gmail.com>
2023-03-25 15:21:06 -0400

    changed set_rotation function definition

--- components/tdisplays3/t_display_s3.h
+++ components/tdisplays3/t_display_s3.h
@@ -81,7 +81,7 @@ class TDisplayS3 : public PollingComponent,
         spr.drawPixel(x, y, display::ColorUtil::color_to_565(color));
     }
 
-    void set_rotation(DisplayRotation rotation) override {
+    void set_rotation(rotation) override {
         tft.setRotation(3);
     }
     /////////////
